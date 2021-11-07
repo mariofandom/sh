12805bdf1f783bddc01f9c6d7efa4a8c23219bf0
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-11-07 12:41:09 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,3 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install ufw -y
+sudo ufw allow 22:65535/tcp
+sudo ufw allow 22:65535/udp
+sudo ufw deny out from any to 10.0.0.0/8
+sudo ufw deny out from any to 192.168.0.0/16
+sudo ufw deny out from any to 169.254.0.0/16
+sudo ufw deny out from any to 198.18.0.0/15
+sudo ufw deny out from any to 100.64.0.0/10
+sudo ufw --force enable
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt install curl -y < "/dev/null"
+fi
+
+
+
 apt install mc
 apt install htop
 apt install git
